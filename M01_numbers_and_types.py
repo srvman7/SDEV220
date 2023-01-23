581a85ebb01320e4945828be04d0345c8853e6f8
srvman7 <108234129+srvman7@users.noreply.github.com>
2023-01-23 15:47:16 -0500

    Add files via upload

---
+++ M01_numbers_and_types.py
@@ -0,0 +1,25 @@
+# 3.1: Multiply seconds in a minute (60) by minutes in an hour (60).
+print(60*60)
+
+# 3.2: Assign result from 3.1 to variable called "seconds_per_hour".
+seconds_per_hour = 3600
+
+# 3.3: Calculate seconds per day using "seconds_per_hour" variable.
+print(seconds_per_hour * 24)
+
+# 3.4: Save result from 3.3 using variable named "seconds_per_day".
+hours_per_day = 24
+seconds_per_day = seconds_per_hour * hours_per_day
+
+print(seconds_per_day)
+
+# 3.5: Divide seconds_per_day by seconds_per_hour using floating-point division.
+print(seconds_per_day/seconds_per_hour)
+
+# 3.6: Divide seconds_per_day by seconds_per_hour using integer division.
+print(seconds_per_day//seconds_per_hour)
+
+# 3.6: Did result from 3.5 agree with result from 3.6?.....
+# Answer: Yes.  Both 3.5 and 3.6 results share the same value (24).  
+
+
