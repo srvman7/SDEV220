453bc3b335e3609b8b9160ccf5b68ceaa72014cb
Kyle Geltmaker <108234129+srvman7@users.noreply.github.com>
2023-03-11 13:50:30 -0500

    Add files via upload

---
+++ M05-Case Study (Docker)/docker/main.py
@@ -0,0 +1,4 @@
+temp_C = float(input("Enter temperature in Celsius: "))  # User inputs temperature in degrees of Celsius.
+temp_F = str(9 / 5 * temp_C + 32)  # Fahrenheit variable for converting Celsius to Fahrenheit.
+
+print("The temperature in Fahrenheit is: " + temp_F)  # Output conversion results.
